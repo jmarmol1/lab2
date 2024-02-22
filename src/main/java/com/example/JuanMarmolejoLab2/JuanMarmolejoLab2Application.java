ad4e5a8c3518f981a131f3c66ed33470b1b179a3
jmarmol1 <jmarmol1@my.centennialcollege.ca>
2024-02-22 15:19:18 -0500

    Create spring app

---
+++ src/main/java/com/example/JuanMarmolejoLab2/JuanMarmolejoLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.JuanMarmolejoLab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanMarmolejoLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanMarmolejoLab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JuanMarmolejoLab2/JuanMarmolejoLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JuanMarmolejoLab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanMarmolejoLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
